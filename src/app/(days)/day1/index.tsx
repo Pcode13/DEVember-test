403d4baede24cc8ba4ce3e955181841031035bb1
Pragati Veer <pragati.veer-v@cms.com>
2024-06-18 02:38:49 +0530

    Stack layout

--- src/app/(days)/day1/index.tsx
+++ src/app/(days)/day1/index.tsx
@@ -1,11 +1,12 @@
 //import liraries
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-
+import { Stack } from 'expo-router';
 // create a component
 const DayDeatilsScreen = () => {
     return (
         <View style={styles.container}>
+            <Stack.Screen options={{ title: 'Day 1' }} />
             <Text>DayDeatilsScreen</Text>
         </View>
     );
