94afb1849067576cc871c1d0a66134b20250e744
Pragati Veer <pragati.veer-v@cms.com>
2024-06-18 03:47:59 +0530

    Ts path

--- src/app/(days)/day1/index.tsx
+++ src/app/(days)/day1/index.tsx
@@ -5,22 +5,18 @@ import { Stack } from 'expo-router';
 // create a component
 const DayDeatilsScreen = () => {
     return (
-        <View style={styles.container}>
-            <Stack.Screen options={{ title: 'Day 1' }} />
-            <Text>DayDeatilsScreen</Text>
-        </View>
+        <View>
+      <Stack.Screen options={{ title: 'Day 1' }} />
+
+      <Text style={{ fontFamily: 'AmaticBold', fontSize: 100 }}>
+        Day Details Screen
+      </Text>
+    </View>
     );
 };
 
 // define your styles
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
-    },
-});
+
 
 //make this component available to the app
 export default DayDeatilsScreen;
